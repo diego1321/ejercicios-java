4c1d82e53efdae5476e759fd7a4504c3f96dec16
diego <diegoalejandrop574@gmail.com>
2022-02-07 22:03:42 -0500

    comitjava

---
+++ Leccion03/03-02 operadoresincremento-decremento.js
@@ -0,0 +1,25 @@
+let a = 3, b = 2;
+let z = a + b;
+
+//incremento
+//Pre-incremento ( el operador ++ antes de la variable)
+z = ++a;
+console.log(a);
+console.log(z);
+
+
+//Post-incremento( el operador ++ despues de la variable)
+z = b++;
+console.log(b);
+console.log(z);
+
+//decremento
+//pre-decremento
+z = --a;
+console.log(a);
+console.log(z);
+
+//post-decremento
+z = b--;
+console.log(b);
+console.log(z);
---
+++ lecccion02/02-01-tiposdedatosjavascript.js
@@ -0,0 +1,4 @@
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Lara";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
