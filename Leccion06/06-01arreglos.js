dc7bdc22c01529f7ffb6a9553808e347b8257c24
diego <diegoalejandrop574@gmail.com>
2022-02-13 23:36:11 -0500

    otrocomit

---
+++ Leccion06/06-01arreglos.js
@@ -0,0 +1,23 @@
+//let autos = new Array('BMW','Mercedez benz','volvo');
+const autos =['BMW','Mercedez benz','volvo'];
+console.log(autos)
+
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i++){
+    console.log(i + ' : ' + autos[i]);
+}
+
+autos[1] = 'mercedesbenz';
+console.log(autos[1]);
+
+autos.push('audi');
+console.log(autos)
+
+console.log(autos.length);
+autos[autos.length] = 'cadillac'
+
+console.log(autos)
+
+
